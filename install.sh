ce6bc4ed443662a21d1f1cbb98e8f88a203232d9
consen <1186508815@qq.com>
2014-11-19 23:21:59 +0800

    update install.sh, use colorful echo.

--- install.sh
+++ install.sh
@@ -5,28 +5,61 @@
 # ~/.vimrc -> from/to/dotfiles/vim/vimrc
 # ~/.tmux.conf -> from/to/dotfiles/tmux/tmux.conf
 # ~/.gitconfig -> from/to/dotfiles/git/gitconfig
-# and install Vim plugins.
 #====================================================
 
 set -e
 
+color_red="\e[0;31m"
+color_green="\e[0;32m"
+color_yellow="\e[0;33m"
+color_blue="\e[0;34m"
+color_end="\e[0m"
+
+echo_ok() {
+    echo -e "  [ ${color_green}OK${color_end} ] $1"
+}
+
+echo_error() {
+    echo -e "  [ ${color_red}ERROR${color_end} ] $1"
+}
+
+echo_user() {
+    echo -e "  [ ${color_yellow}?${color_end} ] $1"
+}
+
+echo_info() {
+    echo -e "  [ ${color_blue}...${color_end} ] $1"
+}
+
+command_exists() {
+    hash "$1" 2>/dev/null
+}
+
+# is OS Ubuntu ?
+install_command() {
+    echo_info "You have not install $1, install it for you."
+    sudo apt-get install $1
+    echo_ok "$1 installed."
+}
+
+
+for tool in git vim tmux; do
+    if ! command_exists $tool; then
+        install_command $tool
+    fi
+done
+
 backup_dir="$HOME/.dotfiles_backup"
 dotfiles="vimrc tmux.conf gitconfig"
 
-# TODO colorful echo
-
-# TODO install git vim tmux
-# is OS Ubuntu ?
-
 # Backup old dotfiles
-echo "Creating $backup_dir for backup of any existing dotfiles in $HOME ..."
+echo_info "Creating $backup_dir for backup of any existing dotfiles in $HOME ..."
 mkdir -p "$backup_dir"
-echo "Done"
 
 for file in $dotfiles; do
     if [[ -f "$HOME/.$file" ]]; then
-    	echo "Moving old .$file from $HOME/ to $backup_dir"
     	mv "$HOME/.$file" "$backup_dir"
+    	echo_ok "Moving old .$file from $HOME/ to $backup_dir"
     fi
 done
 
@@ -37,17 +70,16 @@ echo
 # or any directory out of dotfiles -- ./from/to/dotfiles/install.sh
 script_dir="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Creating symlink ~/.vimrc to $script_dir/vim/vimrc ..."
+echo_info "Installing dotfiles ..."
+
 ln -s "$script_dir/vim/vimrc" "$HOME/.vimrc"
-echo "Done"
+echo_ok "Creating symlink ~/.vimrc to $script_dir/vim/vimrc."
 
-echo "Creating symlink ~/.tmux.conf to $script_dir/tmux/tmux.conf ..."
 ln -s "$script_dir/tmux/tmux.conf" "$HOME/.tmux.conf"
-echo "Done"
+echo_ok "Creating symlink ~/.tmux.conf to $script_dir/tmux/tmux.conf."
 
-echo "Creating symlink ~/.gitconfig to $script_dir/git/gitconfig ..."
 ln -s "$script_dir/git/gitconfig" "$HOME/.gitconfig"
-echo "Done"
+echo_ok "Creating symlink ~/.gitconfig to $script_dir/git/gitconfig."
 
 echo
 
@@ -57,36 +89,23 @@ echo
 
 # Install vim plugin Vundle
 if [[ ! -e ~/.vim/bundle/Vundle.vim ]]; then
-    echo "Installing vim Plugin Vundle ..."
+    echo_info "You have not install vim plugin Vundle, install it for you."
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    echo "Done"
-fi
-
-function command_exists
-{
-    hash "$1" 2>/dev/null
-}
-
-if ! command_exists vim; then
-    echo "Installing Vim ..."
-    sudo apt-get install vim
-    echo "Done"
+    echo_ok "Vundle installed."
 fi
 
 # TODO ignore vim read vimrc error
 
-echo "Using Vundle to install other vim plugins ..."
+echo_info "Using Vundle to install other vim plugins ..."
 vim +VundleInstall +qall
-echo "Done"
 
-echo "Installing exuberant-ctags for vim plugin TagBar ..."
+echo_info "Installing exuberant-ctags for vim plugin TagBar ..."
 sudo apt-get install exuberant-ctags
-echo "Done"
 
-echo "Installing build-essential, python-dev, cmake for compiling YouCompleteMe ..."
+echo_info "Installing build-essential, python-dev, cmake for compiling YouCompleteMe ..."
 sudo apt-get install build-essential python-dev cmake
-echo "Done"
 
-echo "Compiling YouCompleteMe ..."
+echo_info "Compiling YouCompleteMe ..."
 ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
-echo "Done"
+
+echo_ok "All Installed."
